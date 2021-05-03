c0b970843a39bad694540e69ae5f5160b1bdae6c
TRFus-ion <83090151+TRFus-ion@users.noreply.github.com>
2021-05-03 16:19:18 -0400

    Delete script.js

--- assets/js/script.js
+++
@@ -1,7 +0,0 @@
-(function(){
-  const server = io("");
-  const onMessage = function(){};
-  const onPrevData = function(){};
-  server.on("getData",onPrevData);
-  server.on("getMessage",onMessage);
-})();
