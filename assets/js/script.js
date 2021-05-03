79b62a22f9fd7a465e26ab33f21f55dd15b24186
TRFus-ion <83090151+TRFus-ion@users.noreply.github.com>
2021-05-03 12:45:24 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,1 @@
-(function(s,w,c){
-  for(var e = 0; e < s.length; e++){
-    const ss = w.document.createElement("script");
-    ss.src = s[e];
-    ss.onload = c || new Function();
-    w.document.appendChild(ss);
-  }
-})(["https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"],window,function(){
-  return (function(){
-    alert($);
-      $("head").append(`
-       <ul>
-         <li>sheesh</li>
-       </ul>`);
-  })()
-});
+alert();
