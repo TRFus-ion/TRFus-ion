441c06449899d06374be34755caffaf4263aa4d9
TRFus-ion <83090151+TRFus-ion@users.noreply.github.com>
2021-05-03 12:27:29 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,12 @@
-
+(function(s,w,c){
+  for(var e = 0; e < s.length){
+    const ss = w.document.createElement("script");
+    ss.src = s[e];
+    ss.onload = c || new Function();
+    w.document.appendChild(ss);
+  }
+})(["https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"],window,function(){
+  return (function(){
+    alert($);
+  })()
+});
